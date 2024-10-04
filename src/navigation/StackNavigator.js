9f6df8de816c89238e2600638c2cd4d277f29ac8
dhaneswar Setha <dhaneswarsetha.123@gmail.com>
2024-10-04 11:36:39 +0530

    changes

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from '../Pages/LoginScreen';
-import Page1 from '../Pages/Page1';
-import Page2 from '../Pages/Page2';
-import Page3 from '../Pages/Page3';
+import LoginScreen from '../pages/LoginScreen';
+import Page1 from '../pages/Page1';
+import Page2 from '../pages/Page2';
+import Page3 from '../pages/Page3';
 import BottomTabNavigator from './BottomTabNavigator';
-import StudentRegister from '../Pages/StudentRegister';
+import StudentRegister from '../pages/StudentRegister';
 
 const Stack = createNativeStackNavigator();
 

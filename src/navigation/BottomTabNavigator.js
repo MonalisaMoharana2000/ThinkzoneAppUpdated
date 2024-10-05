eaeba0106ae4e4e4d920e230200442770e09500e
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-10-05 16:00:45 +0530

    name changes

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 // import HomeScreen from '../Pages/HomeScreen';
-import Home from '../Pages/Home';
+import Home from '../pages/Home';
 
 const Tab = createBottomTabNavigator();
 

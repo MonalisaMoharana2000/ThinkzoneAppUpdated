e1b620185269655b53ad9c777ab80a8f4b924e16
Swastik Giri <swastikrancodes@gmail.com>
2024-11-01 11:32:43 +0530

    changes in payemwent

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -56,36 +56,36 @@ const Payment = ({route, navigation}) => {
   console.log('teacherdata-------------->', teacherdata);
   // const studentData = useSelector(state => state.UserSlice?.payments);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     if (!teacherdata[0]?.userid) {
-  //       console.warn('User ID is undefined.');
-  //       return;
-  //     }
-  //     console.log(
-  //       'Fetching payment details for user ID:',
-  //       teacherdata[0].userid,
-  //     );
-  //     setIsLoading(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!teacherdata[0]?.userid) {
+        console.warn('User ID is undefined.');
+        return;
+      }
+      console.log(
+        'Fetching payment details for user ID:',
+        teacherdata[0].userid,
+      );
+      setIsLoading(true);
 
-  //     try {
-  //       const response = await API.get(
-  //         `getstudentswithpaymentdetails/${teacherdata[0].userid}`,
-  //       );
-  //       console.log('Fetched payment data:', response.data.data);
-  //       setStudentData(response.data.data);
-  //     } catch (error) {
-  //       console.error('Error fetching payment data:', error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
+      try {
+        const response = await API.get(
+          `getstudentswithpaymentdetails/${teacherdata[0].userid}`,
+        );
+        console.log('Fetched payment data:', response.data.data);
+        setStudentData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching payment data:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  //   fetchData();
-  // }, [teacherdata]);
+    fetchData();
+  }, [teacherdata]);
 
   useEffect(() => {
-    dispatch(fetchUserDataThunk());
+    dispatch(fetchUserDataThunk(teacherdata[0]?.userid));
     // dispatch(studentstypes.getStudentStart(teacherdata[0].userid));
     // API.get(`getstudentswithpaymentdetails/${teacherdata[0].userid}`).then(
     //   response => {
@@ -284,7 +284,7 @@ const Payment = ({route, navigation}) => {
 
   return (
     <>
-      {isLoading ? (
+      {isLoading && !teacherdata[0]?.userid ? (
         <Loading />
       ) : (
         <>
